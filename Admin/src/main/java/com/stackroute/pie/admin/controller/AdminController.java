32b91984f4fdf7ca5c0d77130f409e4c69247f71
achintyapataskar <apataskar780@gmail.com>
2019-02-06 15:38:31 +0530

    Working backend for Admin Service Form operations.

---
+++ Admin/src/main/java/com/stackroute/pie/admin/controller/AdminController.java
@@ -0,0 +1,52 @@
+package com.stackroute.pie.admin.controller;
+
+import com.stackroute.pie.admin.domain.FormFormat;
+import com.stackroute.pie.admin.services.AdminServices;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.Normalizer;
+import java.util.List;
+import java.util.Optional;
+
+@Data
+@Controller
+@RequestMapping("/api/v1/")
+public class AdminController {
+    private AdminServices adminServices;
+    @Autowired
+    public AdminController(AdminServices adminServices) {
+        this.adminServices = adminServices;
+    }
+    @PostMapping("formformat")
+    public ResponseEntity<?> addNewFormFormat(@RequestBody FormFormat formFormat) {
+        FormFormat savedFormFormat = adminServices.addNewFormFormat(formFormat);
+        return new ResponseEntity<FormFormat>(savedFormFormat, HttpStatus.CREATED);
+    }
+    @GetMapping("formformats")
+    public ResponseEntity<?> getAllFormFormats() {
+        List<FormFormat> listOfAllFormFormats = adminServices.getAllFormFormats();
+        return new ResponseEntity<List<FormFormat>>(listOfAllFormFormats, HttpStatus.OK);
+    }
+    @GetMapping("formformat/{formId}")
+    public ResponseEntity<Optional<FormFormat>> getFormFormat(@PathVariable(value = "formId", required = true) int formId) {
+        Optional<FormFormat> retreivedFormFormat = adminServices.getFormFormat(formId);
+        if(retreivedFormFormat == null)
+            return new ResponseEntity<Optional<FormFormat>>(retreivedFormFormat, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<Optional<FormFormat>>(retreivedFormFormat, HttpStatus.FOUND);
+    }
+    @DeleteMapping("formformat/{formId}")
+    public ResponseEntity<?> deleteFormFormat(@PathVariable(value = "formId", required = true) int formId) {
+        adminServices.deleteFormFormat(formId);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+    @PutMapping("formformat/{formId}")
+    public ResponseEntity<FormFormat> updateFormFormat(@PathVariable(value = "formId", required = true) int formId, @RequestBody FormFormat updatedFormFormat) {
+        FormFormat newFormFormat = adminServices.updateFormFormat(formId, updatedFormFormat);
+        return new ResponseEntity<FormFormat>(newFormFormat, HttpStatus.OK);
+    }
+}
