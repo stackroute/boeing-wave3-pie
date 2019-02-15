359c7d73ac30bbdf3c3eff2d92023205bf27d05f
achintyapataskar <apataskar780@gmail.com>
2019-02-15 10:31:43 +0000

    Changed IP to localhost

--- insurer/src/main/java/com/stackroute/pie/config/KafkaConfiguration.java
+++ insurer/src/main/java/com/stackroute/pie/config/KafkaConfiguration.java
@@ -36,7 +36,7 @@ public class KafkaConfiguration {
     @Bean
     public ConsumerFactory<String, Insurer> userConsumerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.23.239.212:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_json");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
