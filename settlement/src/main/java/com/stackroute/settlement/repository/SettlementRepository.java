3631f4055c8c189b942fabae7ae3d58fbe43fa7e
achintyapataskar <apataskar780@gmail.com>
2019-02-19 17:38:20 +0530

    Added query to settlement repository, and reconfigured AdminDashboard.

--- settlement/src/main/java/com/stackroute/settlement/repository/SettlementRepository.java
+++ settlement/src/main/java/com/stackroute/settlement/repository/SettlementRepository.java
@@ -3,8 +3,13 @@ package com.stackroute.settlement.repository;
 import com.stackroute.settlement.domain.PendingTasks;
 import com.stackroute.settlement.domain.Tasks;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SettlementRepository extends MongoRepository<PendingTasks, Integer> {
+    @Query
+    public List<PendingTasks> findAllByInsurerName(String insurerName);
 }
