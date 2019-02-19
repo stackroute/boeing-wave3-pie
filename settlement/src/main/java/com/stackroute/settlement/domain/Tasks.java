5e9edc511d2783c27c1291efc7d0bdd6ffaeaed8
achintyapataskar <apataskar780@gmail.com>
2019-02-19 15:26:13 +0530

    Created data model for Pending Tasks.

--- settlement/src/main/java/com/stackroute/settlement/domain/Tasks.java
+++ settlement/src/main/java/com/stackroute/settlement/domain/Tasks.java
@@ -1,8 +1,14 @@
 package com.stackroute.settlement.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tasks {
+    private String taskName;
+    private String taskDescription;
 }
