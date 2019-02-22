495d9337411bdcaa14809536f76eea850133d331
achintyapataskar <apataskar780@gmail.com>
2019-02-22 15:53:43 +0530

    Display all grievances for each user.

--- settlement/SettlementDashboardForInsurer/src/app/all-porting-requests/all-porting-requests.component.ts
+++ settlement/SettlementDashboardForInsurer/src/app/all-porting-requests/all-porting-requests.component.ts
@@ -1,27 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { FetchPendingTasksService } from '../fetch-pending-tasks.service';
-import { PendingTasks } from '../pending-tasks';
+import { Component, OnInit } from "@angular/core";
+import { FetchPendingTasksService } from "../fetch-pending-tasks.service";
+import { PendingTasks } from "../pending-tasks";
+import { Task } from "../task";
 
 @Component({
-  selector: 'app-all-porting-requests',
-  templateUrl: './all-porting-requests.component.html',
-  styleUrls: ['./all-porting-requests.component.css']
+  selector: "app-all-porting-requests",
+  templateUrl: "./all-porting-requests.component.html",
+  styleUrls: ["./all-porting-requests.component.css"]
 })
 export class AllPortingRequestsComponent implements OnInit {
-
   currentCompanyName: String;
-  pendingTasks: PendingTasks;
+  pendingTasks: PendingTasks[];
+  newPendingTask: Task;
+  currentInsuredName: string;
   fetchAllPortingRequestsIsClicked: Boolean;
+  addANewPendingTaskIsClicked: Boolean;
+  viewPendingTasksOfInsuredIsClicked: Boolean;
   fetchPendingTasksService: FetchPendingTasksService;
-  constructor(fetchPendingTasksService: FetchPendingTasksService) {this.fetchPendingTasksService = fetchPendingTasksService }
+  constructor(fetchPendingTasksService: FetchPendingTasksService) {
+    this.fetchPendingTasksService = fetchPendingTasksService;
+  }
 
   ngOnInit() {
     this.currentCompanyName = null;
     this.fetchAllPortingRequestsIsClicked = false;
+    this.viewPendingTasksOfInsuredIsClicked = false;
+    this.addANewPendingTaskIsClicked = false;
   }
-  fetchAllPortingRequests(currentCompanyName): void{
+  fetchAllPortingRequests(currentCompanyName: string): void {
     this.fetchAllPortingRequestsIsClicked = true;
-    this.fetchPendingTasksService.fetchAllPortingRequests(currentCompanyName).subscribe(pendingTasks => this.pendingTasks = pendingTasks);
+    this.fetchPendingTasksService
+      .fetchAllPortingRequests(currentCompanyName)
+      .subscribe(pendingTasks => (this.pendingTasks = pendingTasks));
   }
-
+  viewPendingTasksOfInsured(insuredName: string): void {
+    this.currentInsuredName = insuredName;
+    this.viewPendingTasksOfInsuredIsClicked = true;
+  }
+  addANewPendingTask(): void {
+    this.addANewPendingTaskIsClicked = true;
+  }
+  saveNewPendingTask(pendingTasksId: number): void {
+    this.newPendingTask.taskStatus = false;
+    this.fetchPendingTasksService
+      .addANewPendingTask(pendingTasksId, this.newPendingTask)
+      .subscribe();
+  }
 }
