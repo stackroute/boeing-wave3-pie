5a498104482211736943e69d52d03ed32e3bcb8e
AnushaVutti <anushavutti1@gmail.com>
2019-02-23 12:10:57 +0530

    Test cases are added for porting service

--- porting/src/main/java/com/stackroute/pie/domain/PortingRequest.java
+++ porting/src/main/java/com/stackroute/pie/domain/PortingRequest.java
@@ -1,5 +1,6 @@
 package com.stackroute.pie.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,13 +11,16 @@ import java.util.Date;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "portingrequest")
 public class PortingRequest {
-private int portingRequestId;
+
+    private int portingRequestId;
     @NotBlank
     private String insuredName;
     @NotBlank
     private String insurerName;
+    //    @NotBlank
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private Date dateOfBirth;
     @NotBlank
@@ -48,6 +52,29 @@ private int portingRequestId;
     private boolean exclusionPeriod;
     private int fromApproval;
     private int toApproval;
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+
+
+    @DateTimeFormat
     private Date acceptedDateofPreviousInsurer;
+
+    public PortingRequest(int portingRequestId, @NotBlank String insuredName, @NotBlank String insurerName, @NotBlank String insuredAddress, @NotBlank String insurerProduct, @NotBlank int sumInsured, @NotBlank int cumulativeBonus, @NotBlank String addOns, @NotBlank int policyNumber, @NotBlank String newInsurerName, @NotBlank String newInsurerProduct, @NotBlank int newSumInsured, @NotBlank int newCumulativeBonus, @NotBlank String reasonForPortability, int familyMembers, @NotBlank boolean exclusionPeriod, int fromApproval, int toApproval) {
+        this.portingRequestId = portingRequestId;
+        this.insuredName = insuredName;
+        this.insurerName = insurerName;
+        this.insuredAddress = insuredAddress;
+        this.insurerProduct = insurerProduct;
+        this.sumInsured = sumInsured;
+        this.cumulativeBonus = cumulativeBonus;
+        this.addOns = addOns;
+        this.policyNumber = policyNumber;
+        this.newInsurerName = newInsurerName;
+        this.newInsurerProduct = newInsurerProduct;
+        this.newSumInsured = newSumInsured;
+        this.newCumulativeBonus = newCumulativeBonus;
+        this.reasonForPortability = reasonForPortability;
+        this.familyMembers = familyMembers;
+        this.exclusionPeriod = exclusionPeriod;
+        this.fromApproval = fromApproval;
+        this.toApproval = toApproval;
+    }
 }
