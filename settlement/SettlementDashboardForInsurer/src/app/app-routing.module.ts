385e410cbec9a4aabee0b6ce65ceb58a7612e5f8
achintyapataskar <apataskar780@gmail.com>
2019-02-25 10:38:38 +0530

    Moved display all porting requests to a different component.

--- settlement/SettlementDashboardForInsurer/src/app/app-routing.module.ts
+++ settlement/SettlementDashboardForInsurer/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DisplayAllPortingRequestsComponent } from './display-all-porting-requests/display-all-porting-requests.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'display-all-porting-requests', component: DisplayAllPortingRequestsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
