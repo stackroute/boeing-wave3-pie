e92f194188b576c3caca4de79128ac356a1ac686
manasa.m <manasa.m@boeing.com>
2019-02-26 14:22:49 +0530

    changed externalinsurerdb service

--- ExternalInsurerDbService/src/main/java/com/stackroute/pie/repository/ExternalDbRepository.java
+++ ExternalInsurerDbService/src/main/java/com/stackroute/pie/repository/ExternalDbRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
- interface ExternalDbRepository extends MongoRepository<Insurer,Long> {
+public interface ExternalDbRepository extends MongoRepository<Insurer,Long> {
 
 
       void deleteByInsurerName(String insurerName);
