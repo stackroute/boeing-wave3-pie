76cb10fd248a18b0d53f4ef91b67f8814f94da1c
parinitamanasa <parinita.manasa@gmail.com>
2019-02-28 14:45:54 +0530

    search controller origins change

--- search-service/src/main/java/com/stackroute/pie/controller/SearchController.java
+++ search-service/src/main/java/com/stackroute/pie/controller/SearchController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/v1/")
 public class SearchController {
