175baa54d54efaa2e641aa6de01263cdd15a2139
Deepak Patwa <deepakgkp192@gmail.com>
2019-03-01 18:13:39 +0530

    tagging and untagging of policy

--- policy/src/main/java/com/stackroute.pie/listener/KafkaConsumer.java
+++ policy/src/main/java/com/stackroute.pie/listener/KafkaConsumer.java
@@ -1,4 +1,35 @@
 package com.stackroute.pie.listener;
 
+import com.stackroute.pie.domain.PortingRequest;
+import com.stackroute.pie.repository.PolicyRepository;
+import com.stackroute.pie.service.PolicyService;
+import io.swagger.models.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class KafkaConsumer {
+    PolicyService policyService;
+    @Autowired
+    KafkaConsumer(PolicyService policyService) {
+        this.policyService = policyService;
+    }
+    @KafkaListener(topics = "incomingporting", groupId = "group1_json", containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(PortingRequest portingRequest){
+        System.out.println("yoyoyoyoyo");
+        System.out.println("Consumed JSON Message: " + portingRequest);
+        String insurerX = portingRequest.getInsurerName();
+        String insurerY = portingRequest.getNewInsurerName();
+        String policyX = portingRequest.getInsurerProduct();
+        String policyY = portingRequest.getNewInsurerProduct();
+        String insuredName = portingRequest.getInsuredName();
+
+        ResponseEntity<?> responseEntity = policyService.deleteInsured(insurerX,policyX,insuredName);
+        System.out.println(responseEntity);
+        ResponseEntity<?> responseEntity1 = policyService.addInsured(insurerY,policyY,insuredName);
+        System.out.println(responseEntity1);
+
+    }
 }
