74ff0264e980c8c38dc63c6e3aa4f22070491a1a
achintyapataskar <apataskar780@gmail.com>
2019-03-08 10:03:23 +0530

    Removed commented fields from PendingTasks.java in domain.

--- settlement/src/main/java/com/stackroute/domain/PendingTasks.java
+++ settlement/src/main/java/com/stackroute/domain/PendingTasks.java
@@ -23,39 +23,4 @@ public class PendingTasks {
     private String insuredName;
     @NotBlank
     private String insurerName;
-
-//    @NotBlank
-//    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-//    private Date dateOfBirth;
-//    @NotBlank
-//    private String insuredAddress;
-//    @NotBlank
-//    private String insurerProduct;
-//    @NotBlank
-//    private int sumInsured;
-//    @NotBlank
-//    private int cumulativeBonus;
-//    @NotBlank
-//    private String[] addOns;
-//    @NotBlank
-//    private int policyNumber;
-//    @NotBlank
-//    private String newInsurerName;
-//    @NotBlank
-//    private String newInsurerProduct;
-//    @NotBlank
-//    private int newSumInsured;
-//    @NotBlank
-//    private int newCumulativeBonus;
-//    @NotBlank
-//    private String reasonForPortability;
-//    private int familyMembers;
-//    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-//    private Date createDate = new Date();
-//    @NotBlank
-//    private boolean exclusionPeriod;
-//    private int fromApproval;
-//    private int toApproval;
-//    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-//    private Date acceptedDateofPreviousInsurer;
 }
