841cb0ef92a07b5503bae54265ec8e5b03fc4e23
sourabhi.tadapatri <sourabhi.tadapatri@boeing.com>
2019-03-11 11:57:10 +0530

    Adding search-service with modifications

--- search-service/src/main/java/com/stackroute/pie/listener/KafkaConsumer.java
+++ search-service/src/main/java/com/stackroute/pie/listener/KafkaConsumer.java
@@ -6,14 +6,8 @@ import com.stackroute.pie.domain.Search;
 import com.stackroute.pie.repository.ChatBotRepository;
 import com.stackroute.pie.repository.SearchRepository;
 import com.stackroute.pie.service.SearchServiceImpl;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -37,27 +31,18 @@ public class KafkaConsumer {
         //consuming data from kafka & setting it to commonauth
         @KafkaListener(topics = "policy_added", groupId = "group_json", containerFactory = "userKafkaListenerFactory")
         public void consumeJson(Policy policy) {
-            System.out.println("yoyoyoyoyo");
-            System.out.println("Consumed JSON Message: " + policy);
             searchRepository.save(policy);
-            System.out.println("Saved Policy" +policy.getPolicyId());
-
-
         }
 
     @KafkaListener(topics = "searchJSON", groupId = "group8_json", containerFactory = "userKafkaListenerFactory")
     public void consumeJson1(Search search) {
-//        System.out.println("yoyoyoyoyo");
-        System.out.println("Consumed JSON Message: " + search);
         String var=search.getSearchString();
         try {
             List<Policy> serch= searchService.tokenString(var);
-
-            System.out.println(serch);
+            chatBotRepository.deleteAll();
             for(Policy p:serch)
             {
                 PolicyList chatPolicy = new PolicyList();
-
                 chatPolicy.setPolicyId(p.getPolicyId());
                 chatPolicy.setPolicyName(p.getPolicyName());
                 chatPolicy.setDiseasesCovered(p.getDiseasesCovered());
@@ -76,7 +61,7 @@ public class KafkaConsumer {
 
         }catch (Exception e)
         {
-            System.out.println("Did not fetch the keywords");
+          e.getMessage();
         }
 
 
--- search-service/src/main/java/com/stackroute/pie/repository/SearchRepository.java
+++ search-service/src/main/java/com/stackroute/pie/repository/SearchRepository.java
@@ -1,11 +1,8 @@
 package com.stackroute.pie.repository;
 
-import com.stackroute.pie.domain.Diseases;
+
 import com.stackroute.pie.domain.Policy;
-import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,8 +17,5 @@ public interface SearchRepository extends MongoRepository<Policy, Integer> {
     //To find policies by InsurerName
     List<Policy> findByInsurerName(String insurerName);
 
-    //To find policies by sumInsured
-//    List<Policy> findBySumInsured(int sumInsured);
-//}
 }
 
