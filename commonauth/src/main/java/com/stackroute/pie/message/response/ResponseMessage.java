5a9d82534b47fa3a18f6f0306b424ab9f0f705d3
Abhishek.Banka <abhishek.banka@boeing.com>
2019-03-11 18:31:07 +0530

    Made changes in the commonauth,insurerreg,policy

--- commonauth/src/main/java/com/stackroute/pie/message/response/ResponseMessage.java
+++ commonauth/src/main/java/com/stackroute/pie/message/response/ResponseMessage.java
@@ -1,19 +1,14 @@
 package com.stackroute.pie.message.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseMessage {
     private String message;
-
-    public ResponseMessage(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
 
 
