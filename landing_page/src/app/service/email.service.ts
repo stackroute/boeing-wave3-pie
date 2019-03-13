fff41635adec425466301b4367d30171786aee19
achintyapataskar <apataskar780@gmail.com>
2019-03-13 12:36:33 +0530

    Changed to AWS Url.

--- landing_page/src/app/service/email.service.ts
+++ landing_page/src/app/service/email.service.ts
@@ -20,6 +20,6 @@ export class EmailService {
 
    }
    sendEmail(email: Email): Observable<Email> {
-      return this.httpClient.post<Email>("http://localhost:8053/api/v1/email/", email, this.httpOptions);
+      return this.httpClient.post<Email>("http://13.126.73.190:8092/email/api/v1/email/", email, this.httpOptions);
    }
 }
