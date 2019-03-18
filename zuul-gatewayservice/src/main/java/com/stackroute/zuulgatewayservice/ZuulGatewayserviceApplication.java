16878ab38e20edae1096c40b18acf410b92f496c
Deepak Patwa <deepakgkp192@gmail.com>
2019-03-18 11:52:01 +0530

    adding one temp controller to zuul

--- zuul-gatewayservice/src/main/java/com/stackroute/zuulgatewayservice/ZuulGatewayserviceApplication.java
+++ zuul-gatewayservice/src/main/java/com/stackroute/zuulgatewayservice/ZuulGatewayserviceApplication.java
@@ -5,12 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 @EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
+@RequestMapping("/api/zuul")
 public class ZuulGatewayserviceApplication {
+
+	@GetMapping("/yo")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulGatewayserviceApplication.class, args);
